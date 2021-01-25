a094da21c3199224c499ee7230e46ac934eded7d
Shashwat Kushwaha <vasuk8324@gmail.com>
2021-01-25 14:27:10 +0530

    Gallery fragment updated

--- app/src/main/java/com/example/iiitp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/iiitp/ui/gallery/GalleryFragment.java
@@ -1,9 +1,16 @@
 package com.example.iiitp.ui.gallery;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,23 +20,95 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.iiitp.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-public class GalleryFragment extends Fragment {
+import java.io.IOException;
 
-    private GalleryViewModel galleryViewModel;
+import static android.app.Activity.RESULT_OK;
+
+class Constants {
+
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
+
+public class GalleryFragment<root> extends Fragment implements View.OnClickListener {
+
+    //private GalleryViewModel galleryViewModel;
+
+    private static final int PICK_IMAGE_REQUEST = 5;
+
+
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private EditText editTextName;
+    private TextView textViewShow;
+    private ImageView imageView;
+
+
+    private Uri filePath;
+
+
+    private StorageReference storageReference;
+    private DatabaseReference mDatabase;
+
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+        // galleryViewModel = new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        buttonChoose = root.findViewById(R.id.buttonChoose);
+        buttonUpload = root.findViewById(R.id.buttonUpload);
+        imageView =  root.findViewById(R.id.imageView);
+        editTextName =  root.findViewById(R.id.editText);
+        textViewShow = root.findViewById(R.id.textViewShow);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference(Constants.DATABASE_PATH_UPLOADS);
+
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+        textViewShow.setOnClickListener(this);
+
         return root;
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == buttonChoose) {
+            showFileChooser();
+        } else if (view == buttonUpload) {
+
+        } else if (view == textViewShow) {
+
+        }
     }
 }
