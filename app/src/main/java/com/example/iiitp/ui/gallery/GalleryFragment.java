c5f741fa058991052e62e4bd5329eac977950b50
SKD <sarthakd762@gmail.com>
2021-01-25 16:39:08 +0530

    Document section added

--- app/src/main/java/com/example/iiitp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/iiitp/ui/gallery/GalleryFragment.java
@@ -1,9 +1,13 @@
 package com.example.iiitp.ui.gallery;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,23 +17,14 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.iiitp.R;
+import com.example.iiitp.ui.documents.Constants;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment{
 
-    private GalleryViewModel galleryViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
     }
 }
