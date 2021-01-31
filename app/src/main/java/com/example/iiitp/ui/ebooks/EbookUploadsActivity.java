8367f0627f7e730a0eb3b171794c3e77a7114fcd
Samarth Bhawsar <samarthbhawsar19@ece.iiitp.ac.in>
2021-01-31 21:22:42 +0530

    updated

---
+++ app/src/main/java/com/example/iiitp/ui/ebooks/EbookUploadsActivity.java
@@ -0,0 +1,141 @@
+package com.example.iiitp.ui.ebooks;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.iiitp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class EbookUploadsActivity extends AppCompatActivity{
+    //the listview
+    ListView listView;
+
+    //database reference to get uploads data
+    DatabaseReference mDatabaseReference;
+
+    //list to store uploads data
+    List<EbookUpload> uploadList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ebooks_uploads);
+
+        uploadList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.mylistView);
+
+        viewAllFiles();
+      Bundle bundle= getIntent().getExtras();
+      if(bundle!= null){
+          if(bundle.getString("some")!=null){
+              Toast.makeText(getApplicationContext(), "data:"+bundle.getString("some"), Toast.LENGTH_SHORT).show();
+          }
+      }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //getting the upload
+                EbookUpload upload = uploadList.get(i);
+
+                //Opening the upload file in browser using the upload url
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(upload.getUrl()));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void viewAllFiles() {
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference(EbookConstants.DATABASE_PATH_UPLOADS);
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    EbookUpload upload = postSnapshot.getValue(EbookUpload.class);
+                    uploadList.add(upload);
+                }
+
+                String[] uploads = new String[uploadList.size()];
+
+                for (int i = 0; i < uploads.length; i++) {
+                    uploads[i] = uploadList.get(i).getName();
+                }
+
+                //displaying it to list
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    //adding a clicklistener on listview
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                //getting the upload
+//                EbookUpload upload = uploadList.get(i);
+//
+//                //Opening the upload file in browser using the upload url
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse(upload.getUrl()));
+//                startActivity(intent);
+//            }
+//        });
+//
+//
+//        //getting the database reference
+//        mDatabaseReference = FirebaseDatabase.getInstance().getReference(EbookConstants.DATABASE_PATH_UPLOADS);
+//
+//        //retrieving upload data from firebase database
+//        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                    EbookUpload upload = postSnapshot.getValue(EbookUpload.class);
+//                    uploadList.add(upload);
+//                }
+//
+//                String[] uploads = new String[uploadList.size()];
+//
+//                for (int i = 0; i < uploads.length; i++) {
+//                    uploads[i] = uploadList.get(i).getName();
+//                }
+//
+//                //displaying it to list
+//                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+//                listView.setAdapter(adapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
+
+
+}
