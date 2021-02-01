b6c65bbe7e19b4db4ce5155cbf8cff0de1807466
Shashwat Kushwaha <vasuk8324@gmail.com>
2021-02-01 11:43:08 +0530

    Gallery view activity is opening

--- app/src/main/java/com/example/iiitp/ui/ebooks/Ebooks.java
+++ app/src/main/java/com/example/iiitp/ui/ebooks/Ebooks.java
@@ -27,7 +27,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.iiitp.MainActivity;
+
 import com.example.iiitp.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
