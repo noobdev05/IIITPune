26390881f922216affd2ece7ec32939bed3d7257
Samarth Bhawsar <samarthbhawsar19@ece.iiitp.ac.in>
2021-03-11 21:27:24 +0530

    new

---
+++ app/src/main/java/com/example/iiitp/FacultyData.java
@@ -0,0 +1,13 @@
+package com.example.iiitp;
+
+public class FacultyData {
+    public int imageId;
+    public String name;
+    public String id;
+
+    public FacultyData(String name, int imageId, String id){
+        this.name = name;
+        this.imageId = imageId;
+        this.id = id;
+    }
+}
